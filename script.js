af899b5c3bb001403f7cc33b403f996de0f3c796
Travis Cook <travismcook93@gmail.com>
2020-09-30 18:20:24 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,102 @@
+var quizStart = document.getElementById("quizStart");
+var timeEl = document.getElementById("time");
+var questionText = document.getElementById("question")
+var answerText = document.getElementById("answer")
+var answerList = document.getElementById("answer-list")
+var loopNum = 0;
+var totalCorrect = 0;
+var currQuestionIdx = 0;
+var quizContent = [
+  {
+    question: "WHAT DOES HTML STAND FOR?",
+    answers: [
+      { text: "Hypertext Mark-up Language", isCorrect: true},
+      { text: "Hoagie with Tomato Meat and Lettuce", isCorrect: false},
+      { text: "Holy Toledo, My Lord!", isCorrect: false},
+      { text: "Hot Tamale Me Like!", isCorrect: false}
+    ]
+  },
+  {
+    question: "what even is html?",
+    answers: [
+      { text: "some stuff", isCorrect: false},
+      { text: "absolute nonsense", isCorrect: true}
+    ]
+  }
+]
+var answerButtons = ["A","B","C","D"]
+
+
+
+var countdownStart = 6;
+function quizCountdown(){
+  
+  var initialCountdown = setInterval(function(){
+  countdownStart--;
+  timeEl.textContent = countdownStart + " Seconds left until start!";
+  if(countdownStart === 0){
+    clearInterval(initialCountdown);
+    displayQuestion();
+    timeEl.textContent = ""
+    quizTimer();
+  }
+  }, 1000)
+  
+}
+function displayQuestion(){
+  var currQuestion = quizContent [currQuestionIdx]
+  questionText.textContent = currQuestion.question; //display the question
+  for(loopNum=0;loopNum<answerButtons.length;loopNum++){ //loop through answers
+    var listItem = document.createElement("li")
+    listItem.textContent = currQuestion.answers[loopNum].text
+    var button = document.createElement("button")
+    button.setAttribute("class", "btn btn-primary")
+    button.setAttribute("isCorrect", currQuestion.answers[loopNum].isCorrect)
+    button.textContent = answerButtons[loopNum]
+    listItem.appendChild(button);
+    answerList.appendChild(listItem)
+    answerList.addEventListener("click", checkAnswer)
+}
+}
+
+function quizTimer(){
+  countdownStart = 300;
+  var secondCountdown = setInterval(function(){
+  countdownStart--;
+  timeEl.textContent = countdownStart + " Seconds left to finish!"; 
+  if(countdownStart === 0){
+    clearInterval(secondCountdown);
+    timeEl.textContent = "Game Over!"
+    timeEl.innerText = ("You got  "+ totalCorrect +" right!")
+  } 
+  },10)
+}
+
+function checkAnswer(e){
+    var isCorrect = e.target.attributes[1].value
+    console.log(isCorrect)
+    switch (isCorrect){
+      case "true":
+        totalCorrect++;
+        currQuestionIdx++
+        questionText.innerHTML = ""
+        answerList.innerHTML = ""
+        displayQuestion()
+        break;
+      case "false":
+        currQuestionIdx++
+        countdownStart--;
+        countdownStart--;
+        countdownStart--;
+        countdownStart--;
+        countdownStart--;
+        questionText.innerHTML = ""
+        answerList.innerHTML =""
+        displayQuestion();
+        break;
+    }
+}
+//local storage
+
+
+quizStart.addEventListener("click", quizCountdown)
